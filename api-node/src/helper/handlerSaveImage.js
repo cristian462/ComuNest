e3a3726d98ade4c450eb63660144688a39d7ce3f
cristian462 <131487492+cristian462@users.noreply.github.com>
2024-05-10 14:32:01 +0200

    Continuación de la memoria y eliminar archivos que no se usan

--- api-node/src/helper/handlerSaveImage.js
+++
@@ -1,9 +0,0 @@
-const fs = require('node:fs');
-
-function saveImage(file,location, nombre){
-	const newPath = `uploads/${location}/${nombre}`;
-	fs.renameSync(file.path, newPath);
-}
-
-
-module.exports = {saveImage};
